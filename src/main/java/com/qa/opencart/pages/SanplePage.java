0a7aa25d75f2975eee1b0b53c787a9f73bbf8c27
Ramachandra Reddy <ramureddy4555@gmail.com>
2025-09-02 08:25:34 +0530

    updated samplepage with new logic and removed some logic

--- src/main/java/com/qa/opencart/pages/SanplePage.java
+++ src/main/java/com/qa/opencart/pages/SanplePage.java
@@ -3,12 +3,9 @@ package com.qa.opencart.pages;
 public class SanplePage {
 	
 	public static void main(String[] args) {
-		int i=10;
 		
-		int x=100;
-		int y=200;
 		
-		System.out.println(x+y);
+		System.out.println("This new file added");
 	}
 
 }
