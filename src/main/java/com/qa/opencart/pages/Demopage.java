3a43a40dfeb48447d6d4cc638dbbfd985fff2090
Ramachandra Reddy <ramureddy4555@gmail.com>
2025-09-03 08:00:10 +0530

    Added demopage

---
+++ src/main/java/com/qa/opencart/pages/Demopage.java
@@ -0,0 +1,12 @@
+package com.qa.opencart.pages;
+
+import org.openqa.selenium.By;
+
+public class Demopage {
+
+	By username= By.name("username");
+	
+	public void entertextToUsername() {
+		System.out.println("username");
+	}
+}
